6d5102e3111a6cb7a59b65c45b90f05b15485f53
Nichole Ezell <nezell8487@gmail.com>
2021-10-30 19:35:51 -0400

    created task-all component to list all tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -21,7 +22,8 @@ import { TaskEditComponent } from './components/task-edit/task-edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
